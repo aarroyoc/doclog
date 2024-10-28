3db06801da97eb0b086fccdd38d6e2b713cf9ae2
Johannes Lötzsch <git@johannesloetzsch.de>
2024-10-28 09:34:09 +0100

    Added watch.sh

---
+++ watch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+## Use inotifywait (from inotify-tools) to rebuild everytime the SOURCE changed.
+## It expects the same 2 arguments as doclog.sh (SOURCE and OUTPUT).
+
+DOCLOG=$(realpath $(dirname $0))
+SOURCE=$1
+OUTPUT=$2
+
+while true; do
+	inotifywait -e modify,create,delete --exclude $OUTPUT -r $SOURCE
+
+	rm -rf $OUTPUT
+	$DOCLOG/doclog.sh $SOURCE $OUTPUT
+done
